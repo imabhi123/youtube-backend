32e626e07efa8c5e2652b15b8d286c65c2c77012
Harry <kumarabhishek13909@gmail.com>
2024-01-23 22:18:19 +0530

    init

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -14,7 +14,7 @@ const generateAccessAndRefreshTokens = async (userId) => {
     user.refreshToken = refreshToken;
     await user.save({ validateBeforeSave: false });
     return { accessToken, refreshToken };
-  } catch (error) {
+  } catch (error){
     throw new ApiError(500, 'Something went wrong while generating refresh and access token');
   }
 }
