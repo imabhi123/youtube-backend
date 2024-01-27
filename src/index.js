e56f5285435c1e884d31672cce0bf8708715d031
Harry <kumarabhishek13909@gmail.com>
2024-01-27 15:52:59 +0530

    added some more controllers like getUserChannelProfile and getWatchHistory

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import dotenv from "dotenv"
 import connectToDB from './db/index.js'
 import {app} from './app.js'
 dotenv.config({
-    path: './.env'
+    path: '../.env'
 })
-
 connectToDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
